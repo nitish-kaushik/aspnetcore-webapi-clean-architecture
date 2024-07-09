59fba18226956ae339f3aea4b113540ee0cd7327
Nitish Kaushik <nitish.webgentle@gmail.com>
2024-07-09 22:10:56 +0800

    Added AspNet Core Clean Architectre Layers

---
+++ MyApp.Api/MyApp.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MyApp.Application;
+using MyApp.Infrastructure;
+
+namespace MyApp.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
---
+++ MyApp.Api/MyApp.Core/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCoreDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
